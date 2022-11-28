b43231c6079fe94335033af742c54ac48d9f052a
gms0817 <gabrielserrano0817@gmail.com>
2022-11-28 07:53:19 -0500

    -atttempting cv

--- main.py
+++ main.py
@@ -4,92 +4,116 @@ import HogTransformer
 import joblib
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 import os
+import PIL
+
+import cvzone
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+
+from PIL import Image
 from skimage.io import imread
 from skimage.transform import resize
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import SGDClassifier
-from sklearn.model_selection import cross_val_predict
 from sklearn.preprocessing import StandardScaler, Normalizer
 
 
-# Read, resize, and store the data into a dictionary
-def resize_all(src, pklname, include, width=150, height=None):
-    height = height if height is not None else width
-
-    # Configure Dictionary
-    data = dict()
-    data['description'] = 'resized ({0}x{1})asl hand-signs in rgb'.format(int(width), int(height))
-    data['label'] = []
-    data['filename'] = []
-    data['data'] = []
-
-    # Configure pkl filename
-    pklname = f'{pklname}_{width}x{height}px.pkl'
+# Rotate the images 5,10,15, and 20 degrees left and right to improve dataset
+def rotate_all(src, include):
     # Read all images in the path and write to a new destination path
     for subdir in os.listdir(src):
         if subdir in include:
             print(f'Loaded "{subdir}"')
             current_path = os.path.join(src, subdir)
 
-            # Populate the dictionary
+            # Rotate the images in the directory and make new files
             for file in os.listdir(current_path):
+                image_path = current_path[0:7] + '/' + subdir + '/' + file
+                new_image_path = current_path[0:7] + '/' + subdir + '/' + file[0:file.index('.')]
                 if file[-3:] in {'jpg', 'png'}:
-                    im = imread(os.path.join(current_path, file))
-                    im = resize(im, (width, height))
-                    data['label'].append(subdir)
-                    data['filename'].append(file)
-                    data['data'].append(im)
+                    current_image = Image.open(image_path)
+                    # Rotate images 5, 10, 15, and 20 degrees left and right
+                    rotated_image = current_image.rotate(-5)  # Rotate -5 degrees
+                    rotated_image.save(new_image_path + '-left-5.png')
 
-        # Store the data
-        joblib.dump(data, pklname)
+                    rotated_image = current_image.rotate(-10)  # Rotate -10 degrees
+                    rotated_image.save(new_image_path + '-left-5.png')
+
+                    rotated_image = current_image.rotate(-15)  # Rotate -15 degrees
+                    rotated_image.save(new_image_path + '-left-15.png')
+
+                    rotated_image = current_image.rotate(-20)  # Rotate -20 degrees
+                    rotated_image.save(new_image_path + '-left-20.png')
+
+                    rotated_image = current_image.rotate(5)  # Rotate 5 degrees
+                    rotated_image.save(new_image_path + '-right-5.png')
+
+                    rotated_image = current_image.rotate(10)  # Rotate 10 degrees
+                    rotated_image.save(new_image_path + '-right-5.png')
+
+                    rotated_image = current_image.rotate(15)  # Rotate 15 degrees
+                    rotated_image.save(new_image_path + '-right-15.png')
+
+                    rotated_image = current_image.rotate(20)  # Rotate 20 degrees
+                    rotated_image.save(new_image_path + '-right-20.png')
+                    print(f'Current Path: {current_path} | File: {file}')
+                    print(f'New Image Path: {new_image_path} | File: {file}')
 
 
-# Main Function
-def main():
-    print("Reached main()")
-    # Set the filepath
-    data_path = 'res/asl'
-    os.listdir(data_path)
+# Read, resize, and store the data into a dictionary
+def resize_all(src, pklname, include, width=150, height=None):
+    print('Reached resize_all().')  # Configure pkl filename
+    pklname = 'asl_80x80px.pkl'
 
-    # Include all the letter folders from ASL dataset
-    include = {'nothing', 'A', 'B', 'C', 'D', 'E', 'F',
-               'G', 'H', 'I', 'J', 'K', 'L',
-               'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-               'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
+    try:
+        data = joblib.load('asl_80x80px.pkl')
+        return data
+    except:
+        height = width
 
-    # Resize all images to 80px by 80px
-    base_name = 'asl'
-    width = 80
+        # Configure Dictionary
+        data = dict()
+        data['label'] = []
+        data['data'] = []
 
-    # Resize / Prep All Images from Dataset (Only needs to be called if pkl hasn't been created)
-    resize_all(src=data_path, pklname=base_name, width=width, include=include)
+        # Read all images in the path and write to a new destination path
+        for subdir in os.listdir(src):
+            if subdir in include:
+                print(f'Loaded "{subdir}"')
+                current_path = os.path.join(src, subdir)
 
-    # Load data from disk and print summary of the data
-    data = joblib.load(f'{base_name}_{width}x{width}px.pkl')
+                # Populate the dictionary
+                for file in os.listdir(current_path):
+                    if file[-3:] in {'jpg', 'png'}:
+                        im = imread(os.path.join(current_path, file))
+                        im = resize(im, (width, height))
+                        data['label'].append(subdir)
+                        data['data'].append(im)
 
-    print('number of samples: ', len(data['data']))
-    print('keys: ', list(data.keys()))
-    print('description: ', data['description'])
-    print('image shape: ', data['data'][0].shape)
-    print('labels:', np.unique(data['label']))
+            # Store the data
+            joblib.dump(data, pklname)
+        return data
 
+
+def plot_dataset(data):
     # Get all unique values in the list of labels
     labels = np.unique(data['label'])
 
     # setup matplotlib figure and axis
     fig, axes = plt.subplots(1, len(labels), figsize=(15, 4))
 
-    # (Can use for presentation of data)
     # make a plot to show examples of each label from dataset
     for ax, label in zip(axes, labels):
         index = data['label'].index(label)
         ax.imshow(data['data'][index])
         ax.axis('off')
         ax.set_title(label)
-    # plt.show() Commented out until needed to store image for presentation
+    plt.show()  # Commented out until needed to store image for presentation
 
+
+def get_classifier(data):
+    print('Reached get_classifier().\nRetrieving Classification Model...')
     # Split data into test set and training set
     # Use 80% for training and remaining data for test-set
     # Use train_test_split since we have multiple 'categories'
@@ -120,7 +144,6 @@ def main():
     X_train_gray = grayify.fit_transform(X_train)
     X_train_hog = hogify.fit_transform(X_train_gray)
     X_train_prepared = scalify.fit_transform(X_train_hog)
-
     # Used for debugging / displaying numerical values of dataset
     # print(X_train_prepared)
 
@@ -128,7 +151,7 @@ def main():
     sgd_clf = SGDClassifier(random_state=42, max_iter=1000,
                             tol=1e-3)  # Random_state is used to recreate the 'random' training
     sgd_clf.fit(X_train_prepared, y_train)  # Train the model with the PREPARED dataset
-
+    print(sgd_clf)
     # Setup Testing Data
     X_test_gray = grayify.transform(X_test)
     X_test_hog = hogify.transform(X_test_gray)
@@ -139,6 +162,94 @@ def main():
 
     print(np.array(y_pred == y_test)[:25])
     print('\nPercentage Correct: ', 100 * np.sum(y_pred == y_test) / len(y_test))
+    joblib.dump(sgd_clf, 'model.sav')
+    return sgd_clf
+
+
+# Main Function
+def main():
+    print("Reached main()")
+
+    # Load / Build Model
+    model = None
+    try:
+        model = joblib.load('model.sav')
+        return model
+    except:
+        print('Preparing Data...')
+        # Set the filepath
+        data_path = 'res/asl'
+        os.listdir(data_path)
+
+        # Include all the letter folders from ASL dataset
+        include = {'nothing', 'A', 'B', 'C', 'D', 'E', 'F',
+                   'del', 'G', 'H', 'I', 'J', 'K', 'L',
+                   'space', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+                   'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
+
+        # Resize all images to 80px by 80px
+        base_name = 'asl'
+        width = 80
+
+        # Resize / Prep All Images from Dataset (Only needs to be called if pkl hasn't been created)
+        # rotate_all(src=data_path, include=include)
+        data = resize_all(src=data_path, pklname=base_name, width=width, include=include)
+
+        # Load data from disk and print summary of the data
+        # data = joblib.load(f'{base_name}_{width}x{width}px.pkl')
+        print('Data prepared.')
+        print('number of samples: ', len(data['data']))
+        print('keys: ', list(data.keys()))
+        print('image shape: ', data['data'][0].shape)
+        print('labels:', np.unique(data['label']))
+        model = get_classifier(data)
+    print('Retrieved Classification Model.')
+
+    print('Loading CV...')
+    # Computer Vision
+    stream = cv2.VideoCapture(0)
+    stream.set(3, 640)
+    stream.set(4, 480)
+
+    grayify = RGB2GrayTransformer.RGB2GrayTransformer()
+    hogify = HogTransformer.HogTransformer(
+        pixels_per_cell=(14, 14),
+        cells_per_block=(2, 2),
+        orientations=9,
+        block_norm='L2-Hys'
+    )
+    detector = HandDetector(detectionCon=0.5, maxHands=1)
+
+    while True:
+        print('CV Running...')
+        data = dict()
+        data['data'] = []
+
+        # Capture frame from video
+        success, img = stream.read()
+
+        # Find the hand and it's landmarks
+        im = detector.findHands(img)
+        lmList, bbox = detector.findPosition(im)
+
+        # Resize image and add to df
+        im = resize(im, (80, 80, 3))
+        data['data'].append(im)
+
+        X_test_gray = grayify.transform(data['data'])
+        X_test_hog = hogify.transform(X_test_gray)
+
+        # print(f'X_test_prepared: {X_test_hog}')
+
+        # Make a Prediction
+        pred = model.predict(X_test_hog)
+        print(pred)
+
+        # Show Frame
+        cv2.imshow('CV', img)
+        cv2.waitKey(10)  # Update every 10 frames
+    # Close program
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
