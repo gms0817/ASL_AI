94bc575948796fc740046c4641cc7a4826eb679e
gms0817 <gabrielserrano0817@gmail.com>
2022-11-29 13:27:45 -0500

    -attempting cv

--- main.py
+++ main.py
@@ -78,6 +78,7 @@ def resize_all(src, pklname, include, width=150, height=None):
         data['data'] = []
 
         # Read all images in the path and write to a new destination path
+        i = 0
         for subdir in os.listdir(src):
             if subdir in include:
                 print(f'Loaded "{subdir}"')
@@ -85,11 +86,13 @@ def resize_all(src, pklname, include, width=150, height=None):
 
                 # Populate the dictionary
                 for file in os.listdir(current_path):
-                    if file[-3:] in {'jpg', 'png'}:
-                        im = imread(os.path.join(current_path, file))
-                        im = resize(im, (width, height))
-                        data['label'].append(subdir)
-                        data['data'].append(im)
+                    while i < 1000:
+                        if file[-3:] in {'jpg', 'png'}:
+                            im = imread(os.path.join(current_path, file))
+                            im = resize(im, (width, height))
+                            data['label'].append(subdir)
+                            data['data'].append(im)
+                        i = i + 1
 
             # Store the data
             joblib.dump(data, pklname)
@@ -112,6 +115,34 @@ def plot_dataset(data):
     plt.show()  # Commented out until needed to store image for presentation
 
 
+def plot_training_results(pass_score_dict, fail_score_dict):
+    print('Reached plot_training_results()')
+
+    # Plot performance
+    plt.rcParams['figure.figsize'] = [7.5, 3.5]
+    plt.rcParams['figure.autolayout'] = True
+
+    # Pass Performance
+    pass_score_dict = np.array(pass_score_dict)
+    x = np.arange(0, len(pass_score_dict))
+    y = pass_score_dict
+    plt.plot(x, y, color="blue", label="Pass")
+
+    # Fail performance
+    fail_score_dict = np.array(fail_score_dict)
+    x_fail = np.arange(0, len(fail_score_dict))
+    y_fail = fail_score_dict
+    plt.plot(x_fail, y_fail, color="red", label="Fail")
+
+    # Customize Scatter Plot
+    plt.title("SGD Classifier Accuracy")
+    plt.xlabel("Number of Samples")
+    plt.ylabel("Accuracy (%)")
+    plt.legend()
+
+    plt.show()  # Show the scatter plot
+
+
 def get_classifier(data):
     print('Reached get_classifier().\nRetrieving Classification Model...')
     # Split data into test set and training set
@@ -124,9 +155,9 @@ def get_classifier(data):
     X_train, X_test, y_train, y_test = train_test_split(
         X,
         y,
-        test_size=0.2,  # 20% Test Size
+        test_size=0.20,  # 20% Test Size
         shuffle=True,  # Randomize samples
-        random_state=42,
+        random_state=100,
     )
 
     # Create instances of RGB2Gray and Hog Transformers
@@ -148,7 +179,7 @@ def get_classifier(data):
     # print(X_train_prepared)
 
     # Train the model using the Stochastic Gradient Descent (SGD) Classifier Model
-    sgd_clf = SGDClassifier(random_state=42, max_iter=1000,
+    sgd_clf = SGDClassifier(random_state=100, max_iter=1000,
                             tol=1e-3)  # Random_state is used to recreate the 'random' training
     sgd_clf.fit(X_train_prepared, y_train)  # Train the model with the PREPARED dataset
     print(sgd_clf)
@@ -159,6 +190,21 @@ def get_classifier(data):
 
     # Train the SGD ASL Recognition Model and Measure Performance
     y_pred = sgd_clf.predict(X_test_prepared)
+    pass_count, fail_count = 0, 0
+    pass_score_dict = []
+    fail_score_dict = []
+    for i in range(0, len(X_test_prepared)):
+        if y_pred[i] == y_test[i]:
+            pass_count = pass_count + 1
+        else:
+            fail_count = fail_count + 1
+        pass_score = pass_count / len(X_test_prepared)
+        pass_score_dict.append(pass_score)
+
+        fail_score = fail_count / len(X_test_prepared)
+        fail_score_dict.append(fail_score)
+    # Plot testing performance
+    plot_training_results(pass_score_dict, fail_score_dict)
 
     print(np.array(y_pred == y_test)[:25])
     print('\nPercentage Correct: ', 100 * np.sum(y_pred == y_test) / len(y_test))
