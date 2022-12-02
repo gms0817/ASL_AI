71ce78ef66fe72ec3857849561c75ed84680ad66
Cymantha Blackmon <blackmoc@kean.edu>
2022-12-01 20:18:48 -0500

    GUI Introduction
    Using CV2, I made a minimal GUI that opens to view the camera and will continue to work on expanding the gui further to make an improved user interface.

---
+++ GUITest.py
@@ -0,0 +1,24 @@
+from tkinter import *
+import cv2
+
+root = Tk()
+
+def openCamera():
+    cap = cv2.VideoCapture(0)
+    while TRUE:
+        ret, frame = cap.read()
+        width = int(cap.get(3))
+        height = int(cap.get(4))
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+        cap.release()
+        cv2.destroyAllWindows()
+
+cameraBtn = Button(root, text="Click to Open Camera", command=openCamera)
+exitBtn = Button(root, text="Exit Camera")
+
+cameraBtn.pack()
+exitBtn.pack()
+root.mainloop()
